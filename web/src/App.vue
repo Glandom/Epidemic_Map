315881c77ddc6a3f5ab6a827db3f65cda00792be
大笨熊 <yunfenggj153t@163.com>
2022-12-11 15:12:20 +0800

    右侧表格部分抽取完成

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div :style="{ background: `url(${bg})` }" class="box">
-    <div style="color:white" class="box-left">
+    <!-- 左侧数据展示区域 -->
+    <div style="color: white" class="box-left">
       <div class="box-left-card">
         <section>
           <div>较上日+ {{ store.chinaAdd.localConfirmH5 }}</div>
@@ -36,43 +37,26 @@
       <div class="box-left-pie"></div>
       <div class="box-left-line"></div>
     </div>
+    <!-- 中间地图展示部分 -->
     <div id="china" class="box-center"></div>
-    <div style="color:white" class="box-right">
-      <table class="table" cellspacing="0" border="1">
-        <thead>
-          <tr>
-            <th>地区</th>
-            <th>新增确诊</th>
-            <th>累计确诊</th>
-            <th>治愈</th>
-            <th>死亡</th>
-          </tr>
-        </thead>
-        <transition-group enter-active-class="animate__animated animate__flipInY" tag="tbody">
-          <tr :key="item.name" v-for="(item, index) in store.item">
-            <td align="center">{{ item.name }}</td>
-            <td align="center">{{ item.today.confirm }}</td>
-            <td align="center">{{ item.total.confirm }}</td>
-            <td align="center">{{ item.total.heal }}</td>
-            <td align="center">{{ item.total.dead }}</td>
-          </tr>
-        </transition-group>
-      </table>
+    <!-- 右侧列表区域  -->
+    <div>
+      <Right></Right>
     </div>
   </div>
 </template>
-  
-<script setup lang='ts'>
+
+<script setup lang="ts">
+// 引入右侧列表组件
+import Right from './components/RightCharts.vue'
 import bg from './assets/1.jpg'
 import { useStore } from './stores'
-import { onMounted } from 'vue';
+import { onMounted } from 'vue'
 import * as echarts from 'echarts' //echarts 5
 import './assets/china'
 import { geoCoordMap } from './assets/geoMap'
-import 'animate.css'
+
 const store = useStore()
-
-
 
 onMounted(async () => {
   await store.getList()
@@ -82,26 +66,26 @@ onMounted(async () => {
 })
 
 const initCharts = () => {
-  const city = store.list.diseaseh5Shelf.areaTree[0].children;
+  const city = store.list.diseaseh5Shelf.areaTree[0].children
   store.item = city[1].children
-  const data = city.map(v => {
+  const data = city.map((v) => {
     return {
       name: v.name,
       value: geoCoordMap[v.name].concat(v.total.nowConfirm),
-      children: v.children
+      children: v.children,
     }
   })
   const charts = echarts.init(document.querySelector('#china') as HTMLElement)
   charts.setOption({
     geo: {
-      map: "china",
+      map: 'china',
       aspectScale: 0.8,
-      layoutCenter: ["50%", "50%"],
-      layoutSize: "100%",
+      layoutCenter: ['50%', '50%'],
+      layoutSize: '100%',
       itemStyle: {
         //  normal: {
         areaColor: {
-          type: "linear-gradient",
+          type: 'linear-gradient',
           x: 0,
           y: 1200,
           x2: 1000,
@@ -109,43 +93,42 @@ const initCharts = () => {
           colorStops: [
             {
               offset: 0,
-              color: "#152E6E", // 0% 处的颜色
+              color: '#152E6E', // 0% 处的颜色
             },
             {
               offset: 1,
-              color: "#0673AD", // 50% 处的颜色
+              color: '#0673AD', // 50% 处的颜色
             },
           ],
           global: true, // 缺省为 false
         },
-        shadowColor: "#0f5d9d",
+        shadowColor: '#0f5d9d',
         shadowOffsetX: 0,
         shadowOffsetY: 15,
         opacity: 0.5,
         // },
-
       },
       emphasis: {
-        areaColor: "#0f5d9d",
+        areaColor: '#0f5d9d',
       },
 
       regions: [
         {
-          name: "南海诸岛",
+          name: '南海诸岛',
           itemStyle: {
-            areaColor: "rgba(0, 10, 52, 1)",
-            borderColor: "rgba(0, 10, 52, 1)",
+            areaColor: 'rgba(0, 10, 52, 1)',
+            borderColor: 'rgba(0, 10, 52, 1)',
             //normal: {
             opacity: 0,
             label: {
               show: false,
-              color: "#009cc9",
+              color: '#009cc9',
             },
             //},
           },
           label: {
             show: false,
-            color: "#FFFFFF",
+            color: '#FFFFFF',
             fontSize: 12,
           },
         },
@@ -153,26 +136,26 @@ const initCharts = () => {
     },
     series: [
       {
-        type: "map",
-        map: "china",
+        type: 'map',
+        map: 'china',
         aspectScale: 0.8,
-        layoutCenter: ["50%", "50%"], //地图位置
-        layoutSize: "100%",
+        layoutCenter: ['50%', '50%'], //地图位置
+        layoutSize: '100%',
         label: {
           show: true,
-          color: "#FFFFFF",
+          color: '#FFFFFF',
           fontSize: 12,
         },
         itemStyle: {
-          areaColor: "#0c3653",
-          borderColor: "#1cccff",
+          areaColor: '#0c3653',
+          borderColor: '#1cccff',
           borderWidth: 1.8,
         },
         emphasis: {
-          areaColor: "#56b1da",
+          areaColor: '#56b1da',
           label: {
             show: true,
-            color: "#fff",
+            color: '#fff',
           },
         },
         data: data,
@@ -184,10 +167,10 @@ const initCharts = () => {
         symbolSize: [45, 45],
         label: {
           show: true,
-          color: "#fff",
+          color: '#fff',
           formatter(value: any) {
             return value.data.value[2]
-          }
+          },
         },
         itemStyle: {
           color: '#1E90FF', //标志颜色
@@ -202,11 +185,13 @@ const initCharts = () => {
 }
 
 const initPie = () => {
-  const charts = echarts.init(document.querySelector('.box-left-pie') as HTMLElement)
+  const charts = echarts.init(
+    document.querySelector('.box-left-pie') as HTMLElement
+  )
   charts.setOption({
-    backgroundColor: "#223651",
+    backgroundColor: '#223651',
     tooltip: {
-      trigger: 'item'
+      trigger: 'item',
     },
     series: [
       {
@@ -215,7 +200,7 @@ const initPie = () => {
         itemStyle: {
           borderRadius: 4,
           borderColor: '#fff',
-          borderWidth: 2
+          borderWidth: 2,
         },
         label: {
           show: true,
@@ -224,58 +209,59 @@ const initPie = () => {
           label: {
             show: true,
             fontSize: '15',
-          }
+          },
         },
-        data: store.cityDetail.map(v => {
+        data: store.cityDetail.map((v) => {
           return {
             name: v.city,
-            value: v.nowConfirm
+            value: v.nowConfirm,
           }
-        })
-      }
-    ]
+        }),
+      },
+    ],
   })
 }
 
 const initLine = () => {
-  const charts = echarts.init(document.querySelector('.box-left-line') as HTMLElement)
+  const charts = echarts.init(
+    document.querySelector('.box-left-line') as HTMLElement
+  )
   charts.setOption({
-    backgroundColor: "#223651",
+    backgroundColor: '#223651',
     tooltip: {
-      trigger: 'axis'
+      trigger: 'axis',
     },
     xAxis: {
       type: 'category',
-      data: store.cityDetail.map(v => v.city),
+      data: store.cityDetail.map((v) => v.city),
       axisLine: {
         lineStyle: {
-          color: "#fff"
-        }
-      }
+          color: '#fff',
+        },
+      },
     },
     yAxis: {
       type: 'value',
       axisLine: {
         lineStyle: {
-          color: "#fff"
-        }
-      }
+          color: '#fff',
+        },
+      },
     },
     label: {
-      show: true
+      show: true,
     },
     series: [
       {
-        data: store.cityDetail.map(v => v.nowConfirm),
+        data: store.cityDetail.map((v) => v.nowConfirm),
         type: 'line',
-        smooth: true
-      }
-    ]
+        smooth: true,
+      },
+    ],
   })
 }
-
 </script>
-  
+
 <style lang="less">
 * {
   padding: 0;
@@ -284,21 +270,6 @@ const initLine = () => {
 @itemColor: #41b0db;
 @itemBg: #223651;
 @itemBorder: #212028;
-.table {
-  width: 100%;
-  background: #212028;
-  tr {
-    th {
-      padding: 5px;
-      white-space: nowrap;
-    }
-    td {
-      padding: 5px 10px;
-      width: 100px;
-      white-space: nowrap;
-    }
-  }
-}
 html,
 body,
 #app {
@@ -342,9 +313,6 @@ body,
   }
   &-center {
     flex: 1;
-  }
-  &-right {
-    width: 450px;
   }
 }
 </style>
---
+++ web/src/components/MiddleMap.vue
@@ -0,0 +1,12 @@
+<template>
+<!-- 中间地图区域 -->
+<div id="china" class="box-center"></div>
+
+</template>
+
+<script setup lang='ts'>
+
+</script>
+<style scoped>
+
+</style>
